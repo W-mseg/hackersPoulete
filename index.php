4ea1f3a497f1d123f9e78cc969a3a930c4175fec
YassBouk <yboukamir@gmail.com>
2020-03-05 14:24:38 +0100

    update

--- index.php
+++ index.php
@@ -57,6 +57,11 @@
             <i class="fab fa-facebook-f"></i>
             </a>
         </li>
+        <li class="nav-item">
+            <a class="nav-link waves-effect waves-light">
+            <i class="fab fa-instagram"></i>
+            </a>
+        </li>
         </ul>
     </div>
     </nav>
@@ -498,7 +503,7 @@
         </li>
         <li class="list-inline-item">
         <a class="btn-floating btn-li mx-1">
-            <i class="fab fa-linkedin-in"> </i>
+            <i class="fab fa-instagram"> </i>
         </a>
         </li>
     </ul>
@@ -515,6 +520,5 @@
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-
 </body>
 </html>
