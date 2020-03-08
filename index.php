101d61ae1717c6dbec837fbc2216b424661ad2da
YassBouk <yboukamir@gmail.com>
2020-03-08 21:56:23 +0100

    front update

--- index.php
+++ index.php
@@ -3,8 +3,7 @@
 <html lang="fr">
     <head>
         <meta charset="UTF-8">
-            <meta name="viewport"
-                content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>HackersPoulette</title>
         <link rel="stylesheet" href="assets/stylesheets/style.css" type="text/css">
@@ -16,13 +15,15 @@
 
     <!--Navbar -->
     <nav class="navbar navbar-expand-lg navbar-dark default-color font-weight-bold">
+
     <a class="navbar-brand" href="#">Hackers Poulette</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-333"
-        aria-controls="navbarSupportedContent-333" aria-expanded="false" aria-label="Toggle navigation">
+            aria-controls="navbarSupportedContent-333" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
+
     <div class="collapse navbar-collapse" id="navbarSupportedContent-333">
-        <ul class="navbar-nav mr-auto">
+    <ul class="navbar-nav mr-auto">
         <li class="nav-item active">
             <a class="nav-link" href="#">Accueil
             <span class="sr-only"></span>
@@ -40,11 +41,11 @@
             <div class="dropdown-menu dropdown-default" aria-labelledby="navbarDropdownMenuLink-333">
             <a class="dropdown-item" href="#quisommesnous">Qui sommes-nous ?</a>
             <a class="dropdown-item" href="#engagements">Nos engagements</a>
-            <a class="dropdown-item" href="#informations">Something else here</a>
+            <a class="dropdown-item" href="#paiement">Moyens de paiement</a>
             </div>
         </li>
-        </ul>
-        <ul class="navbar-nav ml-auto nav-flex-icons">
+    </ul>
+    <ul class="navbar-nav ml-auto nav-flex-icons">
         <li class="nav-item">
             <a class="nav-link waves-effect waves-light">
             <i class="fab fa-twitter"></i>
@@ -60,12 +61,13 @@
             <i class="fab fa-instagram"></i>
             </a>
         </li>
-        </ul>
+    </ul>
     </div>
+
     </nav>
 
     <div class="jumbotron card card-image rounded-0 mb-0" style="background-image: url(assets/raspberry1.jpg);">
-        <div class="text-white text-center py-5 px-4">
+    <div class="text-white text-center py-5 px-4">
     <div>
         <h2 class="card-title h1-responsive pt-3 mb-5 font-bold"><strong>Hackers Poulette</strong></h2>
         <p class="mx-5 mb-5"><b>Basé en Belgique à Charleroi, l'entreprise Hackers Poulette a été créée en 2020 par Yassine BOUKAMIR et Marco SEGRETARIO, elle est devenue très vite un acteur majeur dans le commerce de solutions électroniques.</b></p>
@@ -78,46 +80,46 @@
 
     <main>
         
-        <section id="produits" class="pb-4 mt-0">
+    <section id="produits" class="pb-4 mt-0">
 
         <h2 class="text-center pb-3 pt-3 text-white">Produits</h2>
         
         <!-- Cards -->
         <div class="card-deck mr-0">
-            <div class="card col-12 col-lg-4" data-toggle="modal" data-target="#Modal1">
-                <img class="card-img-top" src=<?= $decodearray[0]['image'];?> alt= "raspberry">
-                <div class="card-body">
-                <h5 class="card-title"><?= $decodearray[0]['name'];?></h5>
-                <p class="card-text"><?= $decodearray[0]['description'];?></p>
-            </div>
-            <div class="card-footer">
-                <small class="text-muted"><?= $decodearray[0]['prix'];?></small>
-            </div>
-            </div>
-            <div class="card col-12 col-lg-4" data-toggle="modal" data-target="#Modal2">
-                <img class="card-img-top" src=<?= $decodearray[1]['image'];?> alt= "raspberry">
-                <div class="card-body">
-                <h5 class="card-title"><?= $decodearray[1]['name'];?></h5>
-                <p class="card-text"><?= $decodearray[1]['description'];?></p>
-            </div>
-            <div class="card-footer">
-                <small class="text-muted"><?= $decodearray[1]['prix'];?></small>
-            </div>
-            </div>
-
-            <div class="card col-12 col-lg-4" data-toggle="modal" data-target="#Modal3">
-                <img class="card-img-top" src=<?= $decodearray[2]['image'];?> alt="raspberry">
-                <div class="card-body">
-                <h5 class="card-title"><?= $decodearray[2]['name'];?></h5>
-                <p class="card-text"><?= $decodearray[2]['description'];?></p>
-            </div>
-            <div class="card-footer">
-                <small class="text-muted"><?= $decodearray[2]['prix'];?></small>
-            </div>
+        <div class="card col-12 col-lg-4" data-toggle="modal" data-target="#Modal1">
+            <img class="card-img-top" src=<?= $decodearray[0]['image'];?> alt= "raspberry">
+            <div class="card-body">
+            <h5 class="card-title"><?= $decodearray[0]['name'];?></h5>
+            <p class="card-text"><?= $decodearray[0]['description'];?></p>
         </div>
-    </div>
+        <div class="card-footer">
+            <small class="text-muted"><?= $decodearray[0]['prix'];?></small>
+        </div>
+        </div>
+        <div class="card col-12 col-lg-4" data-toggle="modal" data-target="#Modal2">
+            <img class="card-img-top" src=<?= $decodearray[1]['image'];?> alt= "raspberry">
+            <div class="card-body">
+            <h5 class="card-title"><?= $decodearray[1]['name'];?></h5>
+            <p class="card-text"><?= $decodearray[1]['description'];?></p>
+        </div>
+        <div class="card-footer">
+            <small class="text-muted"><?= $decodearray[1]['prix'];?></small>
+        </div>
+        </div>
 
-    <div id="more">
+        <div class="card col-12 col-lg-4" data-toggle="modal" data-target="#Modal3">
+            <img class="card-img-top" src=<?= $decodearray[2]['image'];?> alt="raspberry">
+            <div class="card-body">
+            <h5 class="card-title"><?= $decodearray[2]['name'];?></h5>
+            <p class="card-text"><?= $decodearray[2]['description'];?></p>
+        </div>
+        <div class="card-footer">
+            <small class="text-muted"><?= $decodearray[2]['prix'];?></small>
+        </div>
+        </div>
+        </div>
+
+        <div id="more">
 
         <div class="card-deck mr-0">
         <div class="card col-12 col-lg-4" data-toggle="modal" data-target="#Modal4">
@@ -150,11 +152,11 @@
         <div class="card-footer">
             <small class="text-muted"><?= $decodearray[5]['prix'];?></small>
         </div>
-    </div>
-</div>
+        </div>
+        </div>
 
 
-<div class="card-deck mr-0">
+        <div class="card-deck mr-0">
         <div class="card col-12 col-lg-4" data-toggle="modal" data-target="#Modal7">
             <img class="card-img-top" src=<?= $decodearray[6]['image'];?> alt= "raspberry">
             <div class="card-body">
@@ -185,13 +187,13 @@
         <div class="card-footer">
             <small class="text-muted"><?= $decodearray[8]['prix'];?></small>
         </div>
-    </div>
-</div>
-</div>
+        </div>
+        </div>
+        </div>
 
-<div class="row justify-content-center mr-0">
-        <button id="myBtn" onclick="seeMore()">Voir plus</button>
-    </div>
+        <div class="row justify-content-center mr-0">
+            <button id="myBtn" onclick="seeMore()">Voir plus</button>
+        </div>
 
     </section>
 
@@ -407,9 +409,8 @@
                 src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg"
                 alt="Card image cap">
                 <div class="card-body">
-                <h4 class="card-title">Card title</h4>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                    card's content.</p>
+                    <h4 class="card-title">Garantie</h4>
+                    <p class="card-text">Toutes nos garanties commerciales couvrent la panne et proposent des services supplémentaires.</p>
                 </div>
             </div>
             </div>
@@ -420,9 +421,8 @@
                 src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(18).jpg"
                 alt="Card image cap">
                 <div class="card-body">
-                <h4 class="card-title">Card title</h4>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                    card's content.</p>
+                    <h4 class="card-title">Frais de retour</h4>
+                    <p class="card-text">Pour tout colis de poids éligible au départ de Belgique, la prise en charge sera effectuée par un bon postal prépayé.</p>
                 </div>
             </div>
             </div>
@@ -433,9 +433,8 @@
                 src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(35).jpg"
                 alt="Card image cap">
                 <div class="card-body">
-                <h4 class="card-title">Card title</h4>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                    card's content.</p>
+                    <h4 class="card-title">Délai de rétractation</h4>
+                    <p class="card-text">Réservé aux clients particuliers, à compter de la date de livraison de la commande.</p>
                 </div>
             </div>
             </div>
@@ -451,9 +450,8 @@
                 <img class="card-img-top"
                 src="https://mdbootstrap.com/img/Photos/Horizontal/City/4-col/img%20(60).jpg" alt="Card image cap">
                 <div class="card-body">
-                <h4 class="card-title">Card title</h4>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                    card's content.</p>
+                    <h4 class="card-title">Traitement de commande</h4>
+                    <p class="card-text">Généralement, si vous passez et vous réglez votre commande avant 11H, elle sera expédiée le jour même.</p>
                 </div>
             </div>
             </div>
@@ -463,9 +461,9 @@
                 <img class="card-img-top"
                 src="https://mdbootstrap.com/img/Photos/Horizontal/City/4-col/img%20(47).jpg" alt="Card image cap">
                 <div class="card-body">
-                <h4 class="card-title">Card title</h4>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                    card's content.</p>
+                    <h4 class="card-title">Echange en cas de panne</h4>
+                    <h4 class="card-title"></h4>
+                    <p class="card-text">Pour toute panne signalée 7 jours après la livraison d'un produit, nous procédons à un échange immédiat.</p>
                 </div>
             </div>
             </div>
@@ -475,9 +473,8 @@
                 <img class="card-img-top"
                 src="https://mdbootstrap.com/img/Photos/Horizontal/City/4-col/img%20(48).jpg" alt="Card image cap">
                 <div class="card-body">
-                <h4 class="card-title">Card title</h4>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                    card's content.</p>
+                    <h4 class="card-title">Satisfait ou remboursé</h4>
+                    <p class="card-text">Nous proposons à nos client une garantie 15 jours satisfait ou remboursé pour tout nos produits.</p>
                 </div>
             </div>
             </div>
@@ -494,19 +491,19 @@
                 <div class="row">
                     <section id="quisommesnous" class="col-12 col-lg-4 ">
                         <div class="box">
-                            <i class="fas fa-user"> </i>
+                            <i class="fas fa-user fa-3x"></i>
                             <h3>Qui sommes-nous ?</h3>
                             <p>Basé en Belgique à Charleroi, l'entreprise Hackers Poulette a été créée en 2020 par Yassine BOUKAMIR et Marco SEGRETARIO, elle est devenue très vite un acteur majeur dans le commerce de solutions électroniques.</p></div>
                     </section>
                     <section id="engagements" class="col-12 col-lg-4">
                         <div class="box">
-                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star fa-3x"></i>
                             <h3>Nos engagements</h3>
                             <p>Chez nous, le client est important, et nous nous efforçons de toujours vous satisfaire. Nous souhaitons fournir au meilleur prix possible les différents articles présents sur la boutique.</p></div>
                     </section>
-                    <section class="col-12 col-lg-4">
+                    <section id="paiement" class="col-12 col-lg-4">
                         <div class="box">
-                            <i class="fas fa-euro-sign"> </i>
+                            <i class="fas fa-euro-sign fa-3x"></i>
                             <h3>Moyens de paiement</h3>
                             <p>Pour régler une commande, vous disposez de tous les moyens de paiement suivants : cartes bancaires (VISA, MAESTRO, MASTERCARD), Paypal, chèques et virements.</p>
                         </div>
@@ -576,7 +573,7 @@
                 <a href="#engagements">Nos engagements</a>
             </li>
             <li>
-                <a href="#informations">Link 3</a>
+                <a href="#paiement">Moyens de paiement</a>
             </li>
             </ul>
 
@@ -589,18 +586,18 @@
             <!-- Footer Contact -->
             <h5 class="font-weight-bold text-uppercase mt-3 mb-4">Contact</h5>
             <ul class="list-unstyled">
-            <li>
-                <p>
-                    HACKERS-POULETTE<br/>
-                    Square des Martyrs<br/>
-                    6000 CHARLEROI<br/>
-                    BELGIQUE<br/>
-                    Tel : +00 000 00 00
-                </p>
-            </li>
-            <li>
-                <a href="#contact">Contactez-nous</a>
-            </li>
+                <li>
+                    <p>
+                        HACKERS-POULETTE<br/>
+                        Square des Martyrs<br/>
+                        6000 CHARLEROI<br/>
+                        BELGIQUE<br/>
+                        Tel : +00 000 00 00
+                    </p>
+                </li>
+                <li>
+                    <a href="#contact">Contactez-nous</a>
+                </li>
             </ul>
 
         </div>
@@ -633,7 +630,7 @@
         <a href="#"> Hackers-Poulette.com</a>
     </div>
 
-    <!-- Flèche UP -->
+    <!-- Arrow UP -->
         <a href="#" id="arrowup" style="position: fixed;bottom: 0px;right:10px;color: white;font-size: 50px"><i class="fas fa-arrow-up"></i></a>
     </footer>
 
