905357761f91735321c97d59e0e90d2005e95248
W-mseg <58735064+W-mseg@users.noreply.github.com>
2020-03-09 14:40:46 +0100

    update

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <html lang="fr">
     <head>
         <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta name='viewport' content='width=device-width, initial-scale=1'>
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <meta name="Description" content="Site web de Hackers Poulette, une société qui vend des kits et accessoires pour Raspberry Pi à monter soi-même."/>
         <title>HackersPoulette</title>
